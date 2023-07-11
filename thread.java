632aafae8c63ea6cfc271e7c6a3bc3a659967e93
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-11 15:20:35 +0530

    Create thread.java

---
+++ thread.java
@@ -0,0 +1,33 @@
+/* by extending thread class */
+class A extends Thread 
+{
+    @Override
+    public void run()
+    {
+        try
+        {
+            for(int i=1;i<=5;i++)
+            {
+            System.out.println("Abhi");
+            Thread.sleep(1000); //milli second 
+            }
+        }
+        catch(InterruptedException i)
+        {
+
+        }
+    }
+}
+class B
+{
+    public static void main(String[] args) throws InterruptedException {
+        A t=new A();
+        t.start();
+
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Ankush");
+            Thread.sleep(1000);
+        }
+    }
+}
